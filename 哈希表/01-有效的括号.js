0e39215f856bd2434b0316236b28f8cc83fe8451
qingcc <gangqingzhao321@163.com>
2022-04-02 11:44:01 +0800

    前面n个链表

---
+++ 哈希表/01-有效的括号.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function (s) {
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i]
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            stack.push(c)
+        } else {
+            let top = stack[stack.length - 1]
+            if (c === ')' && top === '(' || c === '}' && top === '{' || c === ']' && top === '[') {
+                stack.pop()
+            } else {
+                return false
+            }
+            // return false
+        }
+    }
+    return stack.length === 0
+    /**
+     * 栈的另一种写法
+     */
+    //let stack = []
+    let obj = {
+        '{':'}',
+        '[':']',
+        '(':')'
+    }
+    for(let i = 0;i<s.length;i++){
+        if(s[i]==='{'||s[i]==='('||s[i]==='['){
+            stack.push(s[i])
+        }else {
+            let pop = stack.pop()
+            if(obj[pop]!==s[i]) {
+                return false
+            }
+        }
+    }
+    return stack.length===0
+};
+console.log(isValid("()[]{}"))
