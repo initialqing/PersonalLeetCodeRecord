99328914ffe7267a3b764be0a4334e69448dee98
qingcc <gangqingzhao321@163.com>
2022-04-04 20:36:18 +0800

    每日一题，线段树

--- 动态规划/02-打家劫舍.js
+++ 动态规划/02-打家劫舍.js
@@ -1,10 +1,35 @@
-const dajie  = (nums) => {
-    const max  = (nums,index) => {
-        if (nums===null || index<0) return 0
-        if (index===0) return nums[0]
-        return Math.max(max(nums,index-1),max(nums,index-2)+nums[index])
+// const dajie  = (nums) => {
+//     const max  = (nums,index) => {
+//         if (nums===null || index<0) return 0
+//         if (index===0) return nums[0]
+//         return Math.max(max(nums,index-1),max(nums,index-2)+nums[index])
+//     }
+//     return max(nums,nums.length-1)
+// }
+// let nums = [1,4,3,1,5,3]
+// console.log(dajie(nums))
+/**动态规划
+ * @param {number[]} nums
+ * @return {number}
+ */
+let rob = function(nums) {
+    if (nums.length===0) return 0
+    // let maxVal = 0
+    let dp = []
+    dp[0] = 0
+    dp[1] = nums[0]
+    for(let i = 2;i<nums.length;i++) {
+        dp[i] = Math.max(nums[i-1]+dp[i-2],dp[i-1])
     }
-    return max(nums,nums.length-1)
+    return dp[nums.length]
+};
+/**
+ * 动态规划的另一个版本
+ */
+let robber = (nums) => {
+    let dp = [0]
+    for(let i=0;i<nums.length;i++) {
+        dp[i+1] = Math.max(dp[i],nums[i]+(dp[i-1] ?? 0))
+    }
+    return dp[nums.length]
 }
-let nums = [1,4,3,1,5,3]
-console.log(dajie(nums))
--- 数组/12-区域和检索-数组可修改.js
+++ 数组/12-区域和检索-数组可修改.js
@@ -1,8 +1,19 @@
+class treeNode {
+    constructor(start, end) {
+        this.left = null
+        this.right = null
+        this.sum = null
+        this.start = start
+        this.end = end
+    }
+}
+let root = null
 /**
  * @param {number[]} nums
  */
-const NumArray = function(nums) {
-       this.nums = nums
+const NumArray = function (nums) {
+    this.root = this.buildTree(nums, 0, nums.length - 1)
+    console.log(this.root.sum)
 };
 
 /**
@@ -10,8 +21,8 @@ const NumArray = function(nums) {
  * @param {number} val
  * @return {void}
  */
-NumArray.prototype.update = function(index, val) {
-    this.nums[index] = val
+NumArray.prototype.update = function (index, val) {
+    this.updateVal(this.root, index, val)
 };
 
 /**
@@ -19,14 +30,57 @@ NumArray.prototype.update = function(index, val) {
  * @param {number} right
  * @return {number}
  */
-NumArray.prototype.sumRange = function(left, right) {
-    let sum = 0
-    for (let i = left; i <=right ; i++) {
-        sum+=this.nums[i]
-    }
-    return  sum
+NumArray.prototype.sumRange = function (left, right) {
+    return this.sum(this.root,left,right)
 };
+NumArray.prototype.buildTree = function (nums, start, end) {
+    if (start > end) {
+        return null
+    } else if (start === end) {
+        let node = new treeNode(start, end);
+        node.sum = nums[start]
+        return node
+    } else {
+        let node = new treeNode(start, end);
+        let mid = start + (end - start) / 2
+        mid = Math.floor(mid)
+        node.left = this.buildTree(nums, start, mid)
+        node.right = this.buildTree(nums, mid + 1, end)
+        node.sum = node.left.sum + node.right.sum
+        return node
+    }
+}
+NumArray.prototype.updateVal = function (root, index, val) {
+    if (root.start === root.end) {
+        root.sum = val
+        return
+    } else {
+        let mid = root.start + (root.end - root.start) / 2
+        mid = Math.floor(mid)
+        if (index <= mid) {
+            this.updateVal(root.left, index, val)
+        } else {
+            this.updateVal(root.right, index, val)
+        }
+        root.sum = root.left.sum + root.right.sum
+    }
+}
+NumArray.prototype.sum = function (root, left, right) {
+    if (left === root.start && right === root.end) {
+        return root.sum
+    } else {
+        let mid = root.start + (root.end - root.start) / 2
+        mid = Math.floor(mid)
+        if (right <= mid) {
+            return this.sum(root.left,left,right)
+        } else if (left > mid){
+            return  this.sum(root.right,left,right)
+        }else {
+            return this.sum(root.left,left,mid) + this.sum(root.right,mid+1,right)
+        }
 
+    }
+}
 /**
  * Your NumArray object will be instantiated and called as such:
  * var obj = new NumArray(nums)
