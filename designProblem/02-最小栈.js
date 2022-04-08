46a92204f1f97f5deab54c1bcf5622851908b4a1
qingcc <gangqingzhao321@163.com>
2022-04-08 22:15:00 +0800

    2022-4-8 晚上22:14

---
+++ designProblem/02-最小栈.js
@@ -0,0 +1,51 @@
+const MinStack = function() {
+        this.stack = []
+        this.minVal = -Infinity
+};
+// -2 0 -3 min pop top min
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    // let maxVal = 0
+    if (this.stack.length===0) {
+        this.minVal = val
+        this.stack.push(val)
+    }else {
+        let min = Math.min(...this.stack)
+        if (val<min){
+            this.minVal = val
+        }
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    let index = this.stack.length-1
+    return this.stack[index]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+        return this.minVal
+};
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
