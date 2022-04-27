b107840a62095a38445e87728ac44514ef89345d
qingcc <gangqingzhao321@163.com>
2022-04-27 11:50:59 +0800

    code

--- 2022-4/2022-4_Week2/array/06-下一个排列.js
+++ 2022-4/2022-4_Week2/array/06-下一个排列.js
@@ -3,18 +3,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const nextPermutation = function (nums) {
-    let flag = false
-    for (let i = nums.length - 1; i >= 1; i--) {
-        if (nums[i] > nums[i - 1]) {
-            flag = true
-            let temp = nums[i]
-            nums[i] = nums[i - 1]
-            nums[i - 1] = temp
-            break
-        }
-    }
-    if (!flag) {
-        nums.sort((a,b) => a-b)
-    }
+    
 };
 nextPermutation([1,3,2])
