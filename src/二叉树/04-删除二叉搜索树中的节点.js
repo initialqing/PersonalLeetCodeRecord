d55e5f9ff3580ab81a42e657fa90f91e062e1a05
ZGQ <gangqingzhao321@163.com>
2022-09-26 20:41:49 +0800

    feat:删除二叉搜索树节点

--- src/二叉树/04-删除二叉搜索树中的节点.js
+++ src/二叉树/04-删除二叉搜索树中的节点.js
@@ -3,28 +3,28 @@
  * @param {number} key
  * @return {TreeNode}
  */
-var deleteNode = function(root, key) {
-    if(!root) return root
-    if(root.val === key) {
-        if(!root.left) {
+var deleteNode = function (root, key) {
+    if (!root) return root
+    if (root.val === key) {
+        if (!root.left) {
             return root.right
         }
-        if(!root.right) {
+        if (!root.right) {
             return root.left
         }
-        let cur  = root.right
-        while(cur.left) {
+        let cur = root.right
+        while (cur.left) {
             cur = cur.left
         }
         cur.left = root.left
         root = root.right
         return root
     }
-    if(root.val > key) {
-        root.left = deleteNode(root.left,key)
+    if (root.val > key) {
+        root.left = deleteNode(root.left, key)
     }
-    if(root.val < key) {
-        root.right = deleteNode(root.right,key)
+    if (root.val < key) {
+        root.right = deleteNode(root.right, key)
     }
     return root
 };
--- src/链表/09-同构字符串.ts
+++ src/链表/09-同构字符串.ts
@@ -5,16 +5,16 @@
  */
 function isIsomorphic(s: string, t: string): boolean {
     // 两个map 遍历的时候进行 s[i] 和 t[j]的映射
-    let map1:Map<string,string> = new Map()
-    let map2:Map<string,string> = new Map()
+    let map1: Map<string, string> = new Map()
+    let map2: Map<string, string> = new Map()
 
-    for(let i = 0;i < s.length;i++) {
-        let temp1:string |undefined = map1.get(s[i])
-        let temp2:string | undefined = map2.get(t[i])
-        if(temp1 === undefined && temp2 === undefined) {
-            map1.set(s[i],t[i])
-            map2.set(t[i],s[i])
-        }else if(temp1!==t[i] || temp2 !== s[i]) {
+    for (let i = 0; i < s.length; i++) {
+        let temp1: string | undefined = map1.get(s[i])
+        let temp2: string | undefined = map2.get(t[i])
+        if (temp1 === undefined && temp2 === undefined) {
+            map1.set(s[i], t[i])
+            map2.set(t[i], s[i])
+        } else if (temp1 !== t[i] || temp2 !== s[i]) {
             return false
         }
     }
