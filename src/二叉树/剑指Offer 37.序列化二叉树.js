518006784b8d194829fe11cb3be381ffc05c91b1
ZGQ <gangqingzhao321@163.com>
2022-10-31 08:37:38 +0800

    序列化二叉树

--- src/二叉树/剑指Offer 37.序列化二叉树.js
+++ src/二叉树/剑指Offer 37.序列化二叉树.js
@@ -23,21 +23,21 @@ function deserialize(data) {
     for (let i = 1; i < dataArr.length;) {
         const parentNode = queue.shift()
         const leftNode = dataArr[i++]
-        if(leftNode !== 'null') {
+        if (leftNode !== 'null') {
             const node = new TreeNode(Number(leftNode))
             parentNode.left = node
             queue.push(node)
-        }else {
+        } else {
             parentNode.left = null
         }
         const rightNode = dataArr[i++]
-        if(rightNode !== 'null') {
+        if (rightNode !== 'null') {
             const node = new TreeNode(Number(rightNode))
             parentNode.right = node
             queue.push(node)
-        }else {
+        } else {
             parentNode.right = null
-        } 
+        }
     }
     return root
 }
