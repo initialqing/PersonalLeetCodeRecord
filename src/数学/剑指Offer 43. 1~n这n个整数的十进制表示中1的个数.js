c2a22faa4b9793a71ed5f08bde2ecf2a5fcdd2a3
ZGQ <gangqingzhao321@163.com>
2022-11-05 14:54:51 +0800

    feat:2022/11/5

---
+++ src/数学/剑指Offer 43. 1~n这n个整数的十进制表示中1的个数.js
@@ -0,0 +1,17 @@
+function countDigitOne(n:number) {
+    let count = 0
+    let rem = 0
+    for(let i = 0;n !==0;i++) {
+        let  temp = Math.floor(n/10)
+        if(10 * temp + 1 < n) temp ++
+        else if(10 *  temp + 1 === n) {
+            count += rem + 1
+            console.log(n,temp)
+        }
+        count += (temp * Math.pow(10,i))
+        rem += (n % 10 * Math.pow(10,i))
+        n = Math.floor(n/10)
+    }
+    return count
+};
+countDigitOne(3421)
